6723e89453cbf9c9c3f8df999f3d9dcf1a3d9e76
muukii <m@muukii.me>
2016-09-10 04:25:14 +0900

    Support Carthage

---
+++ Example/Pods/Target Support Files/DecodeContext/DecodeContext-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DecodeContext : NSObject
+@end
+@implementation PodsDummy_DecodeContext
+@end
---
+++ Example/Pods/Target Support Files/DecodeContext/DecodeContext-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double DecodeContextVersionNumber;
+FOUNDATION_EXPORT const unsigned char DecodeContextVersionString[];
+
